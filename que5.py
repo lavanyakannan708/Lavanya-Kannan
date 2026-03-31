157e14a375ed5daef69ced60fe4c0c071674c8f5
lavanyakannan708 <lavanyakannan708@gmail.com>
2026-03-31 19:48:51 +0530

    Create que5.py

---
+++ que5.py
@@ -0,0 +1,16 @@
+logs = input().split()
+
+total_errors = 0
+max_streak = 0
+current_streak = 0
+
+for log in logs:
+    if log == "ERROR":
+        total_errors += 1
+        current_streak += 1
+        max_streak = max(max_streak, current_streak)
+    else:
+        current_streak = 0
+
+print("Total Errors:", total_errors)
+print("Longest Streak:", max_streak)
